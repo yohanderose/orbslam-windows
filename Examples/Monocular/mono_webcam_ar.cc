15fdfd544996cba9c8ec6c41109d4b030d3a0501
Lewis Baker <bakelew@gmail.com>
2017-09-13 20:44:03 +1200

    Removed ROS stuff, converted AR demo into webcam mode, updated CMakeLists to include all my preferred build options.

--- Examples/Monocular/mono_webcam_ar.cc
+++ Examples/Monocular/mono_webcam_ar.cc
@@ -29,24 +29,18 @@
 #include <opencv2/core/core.hpp>
 #include <pangolin/pangolin.h>
 
+#include "ViewerAR.h"
+
 
 using namespace std;
 using namespace cv;
 using namespace ORB_SLAM2;
 
 
-#define radiansToDegrees(angleRadians) (angleRadians * 180.0 / M_PI)
-
-vector<Point3f> worldPoints;
-vector<Point3f> worldPointNormals;
-bool drawNormals = false;
-
 int parseProgramArguments(int argc, const char *argv[]);
 void scaleCalib();
 void scaleIm(Mat &im);
 void settingsFileUpdate(string &filePath, string name, string val);
-void initPangolinARWindow(pangolin::View& viewReal);
-void renderPangolinARFrame(const string& strSettingPath, pangolin::View& viewReal, Mat& pose, Mat& camFrame);
 
 
 string vocabPath;
@@ -58,156 +52,51 @@ int cameraWidth;
 int cameraHeight;
 
 bool loadMap;
+bool bRGB;
 
 VideoCapture cap;
-Mat cameraPose;
+Mat cameraPose, K, DistCoef;
+
+ViewerAR viewerAR;
+thread tViewer;
 
 System *SLAM = NULL;
 
-void KeyNFunction(void)
-{
-	drawNormals = !drawNormals;
-}
 
-void KeySFunction(void)
+void setupViewerAR()
 {
-	worldPoints.clear();
-	worldPointNormals.clear();
-}
+	viewerAR.SetSLAM(SLAM);
+	cv::FileStorage fSettings(settingsPath, cv::FileStorage::READ);
+	bRGB = static_cast<bool>((int)fSettings["Camera.RGB"]);
+	float fps = fSettings["Camera.fps"];
+	viewerAR.SetFPS(fps);
 
-void KeyAFunction(void)
-{
-	worldPoints.clear();
-	worldPointNormals.clear();
+	float fx = fSettings["Camera.fx"];
+	float fy = fSettings["Camera.fy"];
+	float cx = fSettings["Camera.cx"];
+	float cy = fSettings["Camera.cy"];
 
-	if (cameraPose.empty())
-		return;
-	
-	vector<MapPoint *> mapPoints = SLAM->GetAllMapPoints();
-	for (vector<MapPoint *>::iterator mit = mapPoints.begin(); mit != mapPoints.end(); mit++)
+	viewerAR.SetCameraCalibration(fx, fy, cx, cy);
+
+	K = cv::Mat::eye(3, 3, CV_32F);
+	K.at<float>(0, 0) = fx;
+	K.at<float>(1, 1) = fy;
+	K.at<float>(0, 2) = cx;
+	K.at<float>(1, 2) = cy;
+
+	DistCoef = cv::Mat::zeros(4, 1, CV_32F);
+	DistCoef.at<float>(0) = fSettings["Camera.k1"];
+	DistCoef.at<float>(1) = fSettings["Camera.k2"];
+	DistCoef.at<float>(2) = fSettings["Camera.p1"];
+	DistCoef.at<float>(3) = fSettings["Camera.p2"];
+	const float k3 = fSettings["Camera.k3"];
+	if (k3 != 0)
 	{
-		if (*mit == NULL)
-			continue;
-
-		Mat wp = (*mit)->GetWorldPos();
-		worldPoints.push_back(Point3f(
-			wp.at<float>(0, 0),
-			wp.at<float>(1, 0),
-			wp.at<float>(2, 0)));
-
-		Mat n = (*mit)->GetNormal() * 0.05;
-		worldPointNormals.push_back(Point3f(
-			n.at<float>(0, 0),
-			n.at<float>(1, 0),
-			n.at<float>(2, 0)));
+		DistCoef.resize(5);
+		DistCoef.at<float>(4) = k3;
 	}
-}
 
-void renderPangolinARFrame(const string& strSettingPath, pangolin::View& viewReal, Mat& pose, Mat& camFrame)
-{
-	if (!pangolin::ShouldQuit())
-	{
-		Mat camFrameRgb;
-		FileStorage fSettings(strSettingPath, FileStorage::READ);
-		cvtColor(camFrame, camFrameRgb, CV_BGR2RGB);
-
-		pangolin::GlTexture imageTexture(camFrameRgb.cols, camFrameRgb.rows, GL_RGB, false, 0, GL_RGB, GL_UNSIGNED_BYTE);
-		imageTexture.Upload(camFrameRgb.ptr(), GL_RGB, GL_UNSIGNED_BYTE);
-
-		GLfloat znear = 0.01, zfar = 20;
-
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		glViewport(0, 0, cameraWidth, cameraHeight);
-		glMatrixMode(GL_PROJECTION);
-
-		viewReal.Activate();
-		glDisable(GL_DEPTH_TEST);
-		glColor3f(1.0, 1.0, 1.0);
-		imageTexture.RenderToViewport(true);
-
-		if (!pose.empty())
-		{
-			float rxd = radiansToDegrees(atan2f(pose.at<float>(2, 1), pose.at<float>(2, 2)));
-			float ryd = radiansToDegrees(atan2f(-pose.at<float>(2, 0), sqrtf(pow(pose.at<float>(2, 1), 2) + pow(pose.at<float>(2, 2), 2))));
-			float rzd = radiansToDegrees(atan2f(pose.at<float>(1, 0), pose.at<float>(0, 0)));
-			float tx = pose.at<float>(0, 3);
-			float ty = pose.at<float>(1, 3);
-			float tz = pose.at<float>(2, 3);
-
-			GLfloat m[4][4];
-			GLfloat fx = fSettings["Camera.fx"];
-			GLfloat fy = fSettings["Camera.fy"];
-			GLfloat cx = fSettings["Camera.cx"];
-			GLfloat cy = fSettings["Camera.cy"];
-
-			m[0][0] = 2.0 * fx / cameraWidth;
-			m[0][1] = 0.0;
-			m[0][2] = 0.0;
-			m[0][3] = 0.0;
-
-			m[1][0] = 0.0;
-			m[1][1] = -2.0 * fy / cameraHeight;
-			m[1][2] = 0.0;
-			m[1][3] = 0.0;
-
-			m[2][0] = 1.0 - 2.0 * cx / cameraWidth;
-			m[2][1] = 2.0 * cy / cameraHeight - 1.0;
-			m[2][2] = (zfar + znear) / (znear - zfar);
-			m[2][3] = -1.0;
-
-			m[3][0] = 0.0;
-			m[3][1] = 0.0;
-			m[3][2] = 2.0 * zfar * znear / (znear - zfar);
-			m[3][3] = 0.0;
-
-			glLoadIdentity();
-			glMultMatrixf((GLfloat *)m);
-
-			glTranslated(tx, ty, -tz);
-			glRotated(rzd, 0.0, 0.0, 1.0);
-			glRotated(-ryd, 0.0, 1.0, 0.0);
-			glRotated(-rxd, 1.0, 0.0, 0.0);
-
-			glEnable(GL_DEPTH_TEST);
-			pangolin::glDrawColouredCube(-0.05f, 0.05f);
-
-			glPointSize(5.0f);
-			for (int i = 0; i < worldPoints.size(); ++i)
-			{
-				glBegin(GL_POINTS);
-				glColor3f(1.0f, 1.0f, 1.0f);
-				glVertex3f(worldPoints[i].x, worldPoints[i].y, -worldPoints[i].z);
-				glEnd();
-
-				if (drawNormals)
-				{
-					glBegin(GL_LINES);
-					glColor3f(0.0f, 0.0f, 1.0f);
-					glVertex3f(worldPoints[i].x, worldPoints[i].y, -worldPoints[i].z);
-					glVertex3f(worldPoints[i].x + worldPointNormals[i].x, worldPoints[i].y + worldPointNormals[i].y, -worldPoints[i].z + worldPointNormals[i].z);
-					glEnd();
-				}
-			}
-			
-		}
-		else {
-			pangolin::glDrawAxis(1000000.f);
-		}
-
-		// Swap frames and Process Events
-		pangolin::FinishFrame();
-	}
-}
-
-void initPangolinARWindow(pangolin::View& viewReal)
-{
-	pangolin::CreateWindowAndBind("Main", cameraWidth, cameraHeight);
-	glEnable(GL_DEPTH_TEST);
-	viewReal = pangolin::CreateDisplay().SetBounds(0.0, 1.0, 0.0, 1.0, (double)-cameraWidth / (double)cameraHeight);
-	pangolin::RegisterKeyPressCallback('a', KeyAFunction);
-	pangolin::RegisterKeyPressCallback('s', KeySFunction);
-	pangolin::RegisterKeyPressCallback('n', KeyNFunction);
-	return;
+	tViewer = thread(&ORB_SLAM2::ViewerAR::Run, &viewerAR);
 }
 
 
@@ -231,11 +120,11 @@ int main(int argc, const char *argv[])
 	// Create SLAM system. It initializes all system threads and gets ready to process frames.
 	if (loadMap)
 	{
-		SLAM = new System(vocabPath, settingsPath, System::MONOCULAR, true, true);
+		SLAM = new System(vocabPath, settingsPath, System::MONOCULAR, false, true);
 	}
 	else
 	{
-		SLAM = new System(vocabPath, settingsPath, System::MONOCULAR, true, false);
+		SLAM = new System(vocabPath, settingsPath, System::MONOCULAR, false, false);
 	}
 
 	if (SLAM == NULL)
@@ -244,11 +133,10 @@ int main(int argc, const char *argv[])
 		return EXIT_FAILURE;
 	}
 
-	// Set up the opengl AR frame
-	pangolin::View viewReal;
-	initPangolinARWindow(viewReal);
-	
-    cout << endl << "-------" << endl;
+	// Initialise the AR viewer thread
+	setupViewerAR();
+
+	cout << endl << "-------" << endl;
 	cout << "Start processing sequence ..." << endl;
 
 	// Main loop
@@ -257,8 +145,8 @@ int main(int argc, const char *argv[])
 	// From http://stackoverflow.com/questions/19555121/how-to-get-current-timestamp-in-milliseconds-since-1970-just-the-way-java-gets
 	__int64 now = chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now().time_since_epoch()).count();
 
-    while (true)
-    {
+	while (true)
+	{
 		cap >> im;
 
 		if (im.empty() || im.channels() != 3) continue;
@@ -268,18 +156,31 @@ int main(int argc, const char *argv[])
 
 		// Pass the image to the SLAM system
 		cameraPose = SLAM->TrackMonocular(im, curNow / 1000.0);
-		renderPangolinARFrame(settingsPath, viewReal, cameraPose, im);
+
+		Mat imu;
+		int state = SLAM->GetTrackingState();
+		vector<ORB_SLAM2::MapPoint*> vMPs = SLAM->GetTrackedMapPoints();
+		vector<cv::KeyPoint> vKeys = SLAM->GetTrackedKeyPointsUn();
+
+		cv::undistort(im, imu, K, DistCoef);
+		if (bRGB)
+			viewerAR.SetImagePose(imu, cameraPose, state, vKeys, vMPs);
+		else
+		{
+			cv::cvtColor(imu, imu, CV_RGB2BGR);
+			viewerAR.SetImagePose(imu, cameraPose, state, vKeys, vMPs);
+		}
 
 		// This will make a third window with the color images, you need to click on this then press any key to quit
-		imshow("Image", im); 
+		imshow("Image", im);
 		if (waitKey(1) != 255)
 		{
 			break;
 		}
-    }
+	}
 
-    // Stop all threads
-    SLAM->Shutdown();
+	// Stop all threads
+	SLAM->Shutdown();
 	if (!loadMap)
 		SLAM->SaveMap("Slam_Map.bin");
 
@@ -331,7 +232,6 @@ int parseProgramArguments(int argc, const char *argv[])
 	}
 	return 0;
 }
-
 
 
 /* Scales the calibration data to the input video resolution */
@@ -426,3 +326,4 @@ void settingsFileUpdate(string &filePath, string name, string val)
 	remove(filePath.c_str());
 	rename((filePath + ".tmp").c_str(), filePath.c_str());
 }
+
