422f0ee481bbd23265945ed5c7ed24261c05e9cc
Lewis Baker <bakelew@gmail.com>
2017-10-18 13:40:09 +1300

    Update to vs15 2017, add build instructions

--- build.bat
+++ build.bat
@@ -2,14 +2,14 @@ echo "Configuring and building Thirdparty/DBoW2 ..."
 cd Thirdparty/DBoW2
 mkdir build
 cd build
-cmake "-DCMAKE_TOOLCHAIN_FILE=C:/Users/Lewis/MyPrograms/vcpkg/scripts/buildsystems/vcpkg.cmake" -G "Visual Studio 14 2015 Win64" ..
+cmake "-DCMAKE_TOOLCHAIN_FILE=C:/Users/Lewis/MyPrograms/vcpkg/scripts/buildsystems/vcpkg.cmake" -G "Visual Studio 15 2017 Win64" ..
 cmake --build . --config Release
 
 echo "Configuring and building Thirdparty/g2o ..."
 cd ../../g2o
 mkdir build
 cd build
-cmake "-DCMAKE_TOOLCHAIN_FILE=C:/Users/Lewis/MyPrograms/vcpkg/scripts/buildsystems/vcpkg.cmake" -G "Visual Studio 14 2015 Win64" ..
+cmake "-DCMAKE_TOOLCHAIN_FILE=C:/Users/Lewis/MyPrograms/vcpkg/scripts/buildsystems/vcpkg.cmake" -G "Visual Studio 15 2017 Win64" ..
 cmake --build . --config Release
 
 echo "Uncompress vocabulary ..."
@@ -21,5 +21,5 @@ cd ..
 echo "Configuring and building ORB_SLAM2 ..."
 mkdir build
 cd build
-cmake "-DCMAKE_TOOLCHAIN_FILE=C:/Users/Lewis/MyPrograms/vcpkg/scripts/buildsystems/vcpkg.cmake" -G "Visual Studio 14 2015 Win64" ..
+cmake "-DCMAKE_TOOLCHAIN_FILE=C:/Users/Lewis/MyPrograms/vcpkg/scripts/buildsystems/vcpkg.cmake" -G "Visual Studio 15 2017 Win64" ..
 cmake --build . --config Release
